9f365e4b3d5f479dcbd18b8164e70cfc3d692b6d
fujii ryota <lengtaitengjing@gmail.com>
2025-08-29 12:08:13 +0900

    ハートジェム（アイテム）の仕様を実装 ・ゲームパッドの対応 ・アニメーションのコンディションを変更 ・HeartGemタグを追加

--- Assets/Script/EnemyScript/EnemyCon.cs
+++ Assets/Script/EnemyScript/EnemyCon.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class EnemyCon : MonoBehaviour
 {
+    public GameObject enemy = null;
+
+    private void Start()
+    {
+        //GetComponent<EnemyCon>();
+        //enemy.SetActive(true);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //Debug.Log($"{name} ‚ª {other.name} ‚ğUŒ‚");
+            //Debug.Log("“G‚ª‚ ‚½‚Á‚½");
+            //enemy.SetActive(false);
         }
     }
 }
